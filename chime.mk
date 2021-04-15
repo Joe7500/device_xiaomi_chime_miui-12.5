e1b4504c58a82be3bd5fad247e8b2e378a9c995f
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-04-15 14:47:06 +0800

    chime: addressing thermals

--- chime.mk
+++ chime.mk
@@ -208,6 +208,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
+# Init
+PRODUCT_PACKAGES += \
+    init.mi_thermald.rc
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
@@ -505,6 +509,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0
 
 # Touchscreen
 PRODUCT_PACKAGES += \

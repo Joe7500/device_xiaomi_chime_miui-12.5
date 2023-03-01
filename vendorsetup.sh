8a4fea238f0b84ac3be64714c095be8a9530f32b
itsHanibee <itshanibee@gmail.com>
2023-03-01 11:21:53 +0000

    userscript: clone script for building
    makes my life easier
    
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+# ROM source patches
+
+color="\033[0;32m"
+end="\033[0m"
+
+echo -e "${color}Applying patches${end}"
+sleep 1
+
+
+# Clone kernel
+git clone https://github.com/itsHanibee/kernel_xiaomi_chime -b hani kernel/xiaomi/chime
+
+# Clone vendor
+git clone https://gitlab.com/chimeoss/vendor_xiaomi_chime -b lineage-20 vendor/xiaomi/chime
+git clone https://gitlab.com/chimeoss/vendor_xiaomi_citrus -b lineage-20 vendor/xiaomi/citrus
+git clone https://gitlab.com/chimeoss/vendor_xiaomi_lime -b lineage-20 vendor/xiaomi/lime
+
+# Hardware
+rm -rf hardware/xiaomi
+git clone https://github.com/itsHanibee/hardware_xiaomi -b thirteen hardware/xiaomi

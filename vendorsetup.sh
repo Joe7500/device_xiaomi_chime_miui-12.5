26c88e7340428eff26803a8ec3e97a3856530a9c
itsHanibee <itshanibee@gmail.com>
2023-04-24 17:21:54 +0530

    chime: moving to lineage repos for pulling hw
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,9 +6,8 @@ end="\033[0m"
 echo -e "${color}Applying patches${end}"
 sleep 1
 
-
 # Clone kernel
-git clone https://github.com/itsHanibee/kernel_xiaomi_chime -b hani kernel/xiaomi/chime
+git clone https://github.com/itsHanibee/kernel_xiaomi_chime -b main kernel/xiaomi/chime
 
 # Clone vendor
 git clone https://gitlab.com/itsHanibee/vendor_xiaomi_chime -b lineage-20 vendor/xiaomi/chime
@@ -17,4 +16,4 @@ git clone https://gitlab.com/itsHanibee/vendor_xiaomi_lime -b lineage-20 vendor/
 
 # Hardware
 rm -rf hardware/xiaomi
-git clone https://github.com/itsHanibee/hardware_xiaomi -b thirteen hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi

bdb47dc6c8ecd36dbbc65b424233b13dbfaab462
itsHanibee <itshanibee@gmail.com>
2023-04-26 14:39:32 +0530

    chime: switching to AOSP Clang 17
    Signed-off-by: itsHanibee <itshanibee@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,3 +17,16 @@ git clone https://gitlab.com/itsHanibee/vendor_xiaomi_lime -b lineage-20 vendor/
 # Hardware
 rm -rf hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+
+# AOSP Clang 17
+CLANGDIR=$PWD/prebuilts/clang/host/linux-x86
+if [ ! -d "${CLANGDIR}/clang-r487747" ]
+then
+    GREEN='\033[0;32m'
+    NC='\033[0m'
+    echo -e "${GREEN}Downloading clang 17.0.0${NC}"
+    mkdir ${CLANGDIR}/clang-r487747
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r487747.tar.gz -P ${CLANGDIR}
+    tar -C "${CLANGDIR}"/clang-r487747/ -zxf "${CLANGDIR}"/clang-r487747.tar.gz
+    rm ${CLANGDIR}/clang-r487747.tar.gz
+fi 

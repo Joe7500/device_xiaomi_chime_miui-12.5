362c7bf0ce86e031c6997607cd7d798dd794f15d
Joe7500 <user@localhost>
2024-07-11 14:27:56 +0200

    chime: bringup YAAP

---
+++ yaap_chime.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from chime device
+$(call inherit-product, device/xiaomi/chime/device.mk)
+
+# Inherit some common Bootleggers stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := yaap_chime
+PRODUCT_DEVICE := chime
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SM6115
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+TARGET_BUILD_GAPPS := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Maintainer
+#DEVICE_MAINTAINERS := Joe

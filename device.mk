fd9d2b22cc5d5e7afb676dd4fb93c55c476bc325
Joe7500 <joe75001@gmail.com>
2024-08-13 01:17:58 +0200

    Revert "chime: Use common libqti-perfd-client and power-libperfmgr"
    This reverts commit 04a9eb4ac9be1de16f7c5558ec8caf426492d166.

--- device.mk
+++ device.mk
@@ -20,11 +20,8 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
-    hardware/qcom-caf/common/libqti-perfd-client \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Product Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
@@ -348,8 +345,7 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 
 # Perf
 PRODUCT_PACKAGES += \
-    libqti-perfd-client \
-    android.hardware.power-service.lineage-libperfmgr
+    libqti-perfd-client
 
 #PRODUCT_PACKAGES += \
 #    vendor.qti.hardware.perf@2.2 \
@@ -360,6 +356,12 @@ PRODUCT_PACKAGES += \
 #PRODUCT_COPY_FILES += \
 #    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/perf/,$(TARGET_COPY_OUT_VENDOR)/etc)
 #    $(LOCAL_PATH)/perf/poweropt-service-disable.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/poweropt-service-disable.rc
+
+#PRODUCT_PACKAGES += \
+#    vendor.qti.hardware.perf@2.2 \
+#    vendor.qti.hardware.perf@2.2.vendor \
+#    vendor.qti.hardware.servicetracker@1.0.vendor \
+#    vendor.qti.hardware.servicetracker@1.1.vendor
 
 #PRODUCT_COPY_FILES += \
 #    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/perf/,$(TARGET_COPY_OUT_VENDOR)/etc) 
